c14aa46b7de85a3c1d609e78b4b39e558cb34c86
David D. Hua <dhd12391@gmail.com>
2013-11-26 23:44:38 -0800

    rafael's side... backup

---
+++ connectvars.php
@@ -0,0 +1,8 @@
+
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'sfsuswe.com');
+  define('DB_USER', 'rsanch');
+  define('DB_PASSWORD', 'ASDasdqwe');
+  define('DB_NAME', 'student_rsanch');
+?>
